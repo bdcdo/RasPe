b1009ca75a2df1838c506692164f1bcb163ae8e6
Bruno da C. de Oliveira <bruno.oliveira.pgsp@gmail.com>
2025-07-04 22:19:25 -0300

    troquei para scraper_br

--- src/buscraper/__init__.py
+++ src/buscraper/__init__.py
@@ -2,6 +2,6 @@ from importlib.metadata import version
 from .scraper_manager import scraper
 from .utils import expand, remove_duplicates, extract, check
 
-__version__ = version("SERPScraper")
+__version__ = version("scraper_br")
 
 __all__ = ["scraper", "expand", "remove_duplicates", "extract", "check"]
